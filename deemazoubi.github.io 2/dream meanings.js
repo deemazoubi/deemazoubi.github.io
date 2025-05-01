e0c6359f4a9bbc3223505070812774e41e279c48
deemazoubi <daa9707@nyu.edu>
2025-05-01 10:29:19 +0400

    deleted

--- deemazoubi.github.io 2/dream meanings.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("dream-meanings-btn").addEventListener("click", function() {
-  window.location.href = "dream_meanings.html";
-});
-
-document.getElementById("back-btn").addEventListener("click", function() {
-  window.location.href = "index.html"; 
-});
